5626720617055d372ed8a6b7d67c2e1f97865351
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2017-07-04 17:37:07 -0500

    Add minimal menu template

--- main/app.js
+++ main/app.js
@@ -1,12 +1,15 @@
 const electron = require("electron");
-const { app } = electron;
+const { app, Menu } = electron;
 const path = require("path");
 const window = require("./window");
+const { menuTemplate } = require("./menu");
 
 let win;
 
 app.on("ready", () => {
   win = window.createWindow();
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 });
 
 app.on("window-all-closed", function() {
---
+++ main/menu.js
@@ -0,0 +1,61 @@
+const template = [
+  {
+    label: "View",
+    submenu: [
+      { role: "reload" },
+      { role: "forcereload" },
+      { role: "toggledevtools" },
+      { type: "separator" },
+      { role: "resetzoom" },
+      { role: "zoomin" },
+      { role: "zoomout" },
+      { type: "separator" },
+      { role: "togglefullscreen" }
+    ]
+  },
+  {
+    role: "window",
+    submenu: [{ role: "minimize" }, { role: "close" }]
+  },
+  {
+    role: "help",
+    submenu: [
+      {
+        label: "Learn More",
+        click() {
+          require("electron").shell.openExternal(
+            "https://github.com/saisandeepvaddi/whatsapp-green"
+          );
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform === "darwin") {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      { role: "about" },
+      { type: "separator" },
+      { role: "services", submenu: [] },
+      { type: "separator" },
+      { role: "hide" },
+      { role: "hideothers" },
+      { role: "unhide" },
+      { type: "separator" },
+      { role: "quit" }
+    ]
+  });
+
+  // Window menu
+  template[3].submenu = [
+    { role: "close" },
+    { role: "minimize" },
+    { role: "zoom" },
+    { type: "separator" },
+    { role: "front" }
+  ];
+}
+
+exports.menuTemplate = template;
