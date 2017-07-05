5796e0b738e9c98e36e9dfa6189bf1502c64ef82
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2017-07-05 00:35:18 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # my-electron-react-boilerplate
-Electron boilerplate with react configured via webpack and babel presets
+Electron boilerplate with react and redux pre-configured
+
+# What is pre-configured
+- Webpack
+- Basic Redux boilerplate
+- Persistence storage with [electron-settings][1]. Can be disabled easily if not needed. Check comments in the reducer file in `renderer/reducers/index.js`
+
+# Install
+
+- Install `Electron`
+
+```shell 
+  yarn global add electron
+```
+
+- Clone this repository
+
+```shell
+  git clone https://github.com/saisandeepvaddi/electron-react-redux-boilerplate.git
+```
+
+- Have two terminals opened. One for Webpack build. Another for Electron
+
+```shell
+  terminal_1> yarn webpack:dev
+  terminal_2> yarn start
+```
+
+- Devtron is included. Run the following in application's DevTools console
+
+```javascript
+  require('devtron').install()
+```
+
+
+# License
+
+[MIT](https://github.com/saisandeepvaddi/electron-react-redux-boilerplate/blob/master/LICENSE) 
+
+
+
+[1]: https://www.npmjs.com/package/electron-settings
--- renderer/reducers/index.js
+++ renderer/reducers/index.js
@@ -5,18 +5,26 @@ const settings = require("electron-settings");
 
 // Initial State
 const INITIAL_STATE = {
-  sample: "sample"
+  // sample: "sample"
 };
 
-// If persisted state is empty. i.e. first-time use of application
-if (Object.keys(settings.get("state")).length === 0) {
+// WARNING
+// TODO: Remove this later. Strictly for fast debugging of persisted state from browser console.
+// MUST be removed in production
+// Delete if state is not needed to be saved to disk.
+// Usage (in browser console) :
+// To reset to initial state :- console> settings.set("state", {})
+// To get state :- console> settings.get("state.sample")
+window.settings = settings;
+
+// Remove if state is not needed to be saved to disk.
+// If persisted state is empty. Example: first-time use of application
+if (
+  settings.get("state") === undefined ||
+  Object.keys(settings.get("state")).length === 0
+) {
   settings.set("state", INITIAL_STATE);
 }
-
-// WARNING
-// TODO: Remove this later. Strictly for fast debugging purpose in browser console.
-// Delete if settings are not being saved to disk.
-window.settings = settings;
 
 // change to state = INITIAL_STATE if state is not being stored to disk
 const rootReducer = (state = settings.get("state"), action) => {
